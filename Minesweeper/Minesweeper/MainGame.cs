d92bef49af75eefb9343156c9539294e2c9a0ab3
Kartikkumar <krao24@gmail.com>
2015-08-19 07:57:32 -0700

    Initial Working game

---
+++ Minesweeper/Minesweeper/MainGame.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Minesweeper
+{
+    class MainGame
+    {
+        private char m_cOption;
+        private int m_X;
+        private int m_Y;
+        private char[][] m_cField;
+        private char[][] m_cDisplayGrid;
+        private int m_nRows;
+        private int m_nColumns;
+        private bool m_BGameOver;
+        public MainGame()
+        {
+          
+        }
+
+        public MainGame(int r, int c)
+        {
+            m_BGameOver = false;
+            m_cOption = 'o';
+            m_X = m_Y = 0;
+            setRows(r);
+            setColumns(c);
+          
+            InitField();
+        }
+
+        public void UpdateField(char op, int x, int y)
+        {
+            setOption(op);
+            setX(x);
+            setY(y);
+            UpdateGrid();
+        }
+
+        private void setOption(char option)
+        {
+            m_cOption = option;
+        }
+
+        private void setX(int x)
+        {
+            m_X = x;
+
+        }
+
+        private void setY(int y)
+        {
+            m_Y = y;
+        }
+
+
+        public void setRows(int r)
+        {
+            m_nRows = r;
+        }
+
+        public void setColumns(int c)
+        {
+            m_nColumns = c;
+        }
+
+        public void setField(int index, string values)
+        {
+            
+                for(int j = 0; j < m_nColumns; ++j)
+                {
+                    m_cField[index][j] = values[j];
+                }
+            
+        }
+
+
+        public bool gamecomplete()
+        {
+            return (m_cField[m_nRows - 1][ m_nColumns - 1] == '0');
+        }
+        public void InitField()
+        {
+            m_cField = new char[m_nRows][];
+            m_cDisplayGrid = new char[m_nColumns][];
+
+            for (int i = 0; i < m_nRows; ++i)
+            {
+                m_cField[i] = new char[m_nColumns];
+                m_cDisplayGrid[i] = new char[m_nColumns];
+            }
+
+
+            for (int i = 0; i < m_nRows; ++i)
+            {
+                for (int j = 0; j < m_nColumns; ++j)
+                {
+                    m_cField[i][j] = '0';
+                    m_cDisplayGrid[i][j] = 'x';
+                }
+            }
+        }
+
+        public void DisplayGrid()
+        {
+            for(int i = 0 ; i < m_nRows; ++i)
+            {
+                for(int j = 0 ; j < m_nColumns ; ++j)
+                {
+                    Console.Write(m_cField[i][j]);
+                }
+                Console.WriteLine();
+            }
+        }
+
+        public void DisplayGrid2()
+        {
+            for (int i = 0; i < m_nRows; ++i)
+            {
+                for (int j = 0; j < m_nColumns; ++j)
+                {
+                    Console.Write(m_cDisplayGrid[i][j]);
+                }
+                Console.WriteLine();
+            }
+        }
+
+        public bool IsGameover()
+        {
+            return m_BGameOver;
+        }
+
+        public void UpdateGrid()
+        {
+            if (m_cOption == 'o')
+            {
+                if (m_cField[m_X][m_Y] == 'm')
+                {
+                    // you lose the game
+                    m_BGameOver = true;
+                    DisplayGrid();
+                }
+                else
+                {
+                    int val = m_cField[m_X][m_Y] -'0';
+                    val += (m_cField[m_X - 1][m_Y] - '0') +
+                            (m_cField[m_X + 1][m_Y] - '0') +
+                            (m_cField[m_X][m_Y - 1] - '0') +
+                            (m_cField[m_X][m_Y + 1] - '0');
+
+                    m_cField[m_X][m_Y] = Convert.ToChar(val);
+                    m_cDisplayGrid[m_X][m_Y] = Convert.ToChar(val); 
+                    DisplayGrid2();
+                }
+            }
+            else
+            {
+                m_cField[m_X][m_Y] = m_cOption;
+                m_cDisplayGrid[m_X][m_Y] = m_cOption;
+                DisplayGrid2();
+            }
+        }
+    }
+}
---
+++ Minesweeper/Minesweeper/Parser.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Minesweeper
+{
+
+    class Parser
+    {
+        private readonly string m_CSInput;
+        private int m_nRows;
+        private int m_nColumns;
+
+        public MainGame game;
+
+        public Parser(string input)
+        {
+           
+            m_CSInput = input;
+            m_nColumns = m_nRows = 0;
+        }
+
+        public Parser()
+        {
+            m_nColumns = m_nRows = 0;
+        }
+
+        public void ParseInput(string input)
+        {
+            int l = input.Length;
+
+            string[] col = input.Split(',');
+
+            m_nRows = col.Length + 2;
+            
+            m_nColumns = col[0].Length + 2;
+
+            game = new MainGame(m_nRows,m_nColumns);
+            
+            for(int i = 0 ; i < m_nRows; ++i)
+            {
+                game.setField(i,col[i]);
+            }
+
+        }
+
+        public void PlayGame()
+        {
+            string sLayout;
+            Console.WriteLine("Welcome to Minesweeper");
+            Console.Write("Enter the minefield layout : ");
+            sLayout = Console.ReadLine();
+            
+            ParseInput(sLayout);
+          
+
+           while( !game.IsGameover()  && !game.gamecomplete())
+           {
+               string options;
+               Console.Write("Enter option : ");
+               options = Console.ReadLine();
+               ParseOption(options);
+              
+           }
+
+           if (game.IsGameover())
+               Console.WriteLine("Oops, you stepped on a mine ! Game over!");
+
+           else if (game.gamecomplete())
+               Console.WriteLine("Wow,you cleared the minefield ! Game over !");
+
+        }
+
+
+
+        public int GetNumberOfRows()
+        {
+            return m_nRows;
+        }
+
+        public int GetNumberOfColumns()
+        {
+            return m_nColumns;
+        }
+
+        public void ParseOption(string options)
+        {
+            char c = options[0];
+            switch(c)
+            {
+                case 'o':
+                    game.UpdateField(c, options[2] - '0', options[4] - '0'); 
+
+                    break;
+                case 'f':
+                    game.UpdateField(c, options[2] - '0', options[4] - '0');
+                    break;
+            }
+
+        }
+    }
+}
